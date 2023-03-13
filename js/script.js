66e82c71a1371236cee98c869c5c26722bddfe1b
cioby09 <claudiociobanu09@gmail.com>
2023-03-13 22:31:03 +0100

    Add: Bonus

--- js/script.js
+++ js/script.js
@@ -2,16 +2,26 @@ const gridElem = document.querySelector(".grid");
 for (let i = 1; i <=100; i++) {
 
     let result;
+    let classColor;
     if ((i % 3 === 0) && (i % 5 === 0)) {
         result = "FizzBuzz";
+        classColor = "FBcolor";
     } else if (i % 5 === 0) {
         result = "Buzz";
+        classColor = "Bcolor";
     } else if (i % 3 === 0) {
         result = "Fizz";
+        classColor = "Fcolor";
     } else {
         result = i;
+        classColor = "main-color";
     }
 
     console.log(result);
+    gridElem.innerHTML += `<div class="box ${classColor}">${result}</div>`;
+    const newBox = document.createElement("div");
+    newBox.innerHTML = result ;
+    newBox.classList.add("box");
+    newBox.classList.add(classColor);
 
 }
